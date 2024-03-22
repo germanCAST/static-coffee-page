bc3f778d39db99028fda224e091531a21fba878c
german <germancastellanose@gmail.com>
2024-03-22 16:22:33 -0600

    addCoffee --nonf---

---
+++ src/app/pages/add-coffee/add-coffee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-coffee',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-coffee.component.html',
+  styleUrl: './add-coffee.component.css'
+})
+export class AddCoffeeComponent {
+
+}
