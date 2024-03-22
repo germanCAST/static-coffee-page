9fd06c06561b35c401dd7f4a211ea483b3cf7fa1
german <germancastellanose@gmail.com>
2024-03-22 16:38:00 -0600

    :s app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AddCoffeeComponent } from './pages/add-coffee/add-coffee.component';
 
-export const routes: Routes = [{ path: '***', component: AppComponent }];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'addCoffee', component: AddCoffeeComponent },
+];
