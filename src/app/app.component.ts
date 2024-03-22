c85cd568645eaa8e63055a741e00390041bd1d13
german <germancastellanose@gmail.com>
2024-03-22 12:58:18 -0600

    API resp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -79,4 +79,19 @@ export class AppComponent {
       available: true,
     },
   ];
+
+  // API
+
+  async getList() {
+    const baseUrl: string =
+      'https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json';
+    await fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
